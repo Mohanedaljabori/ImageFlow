8531ab9d299d780db86e14632bff1d312cfce5da
Mohaned <mohaned.al-jabori@hva.nl>
2025-08-04 18:32:35 +0200

    deploy update to push docker images to docker hub

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,20 +8,3 @@ terraform {
 }
 
 provider "docker" {}
-
-resource "docker_image" "backend" {
-  name = "spring-backend"
-  build {
-    context = "../file-upload-backend"
-    dockerfile = "Dockerfile"
-  }
-}
-
-resource "docker_container" "backend" {
-  name  = "spring-backend"
-  image = docker_image.backend.name
-  ports {
-    internal = 8080
-    external = 8080
-  }
-}
